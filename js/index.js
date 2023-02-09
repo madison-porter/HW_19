66e9910af55df08a6ef616d99a7f0706389f2787
madison-porter <mnporter12@gmail.com>
2023-02-09 13:30:52 -0800

    updates to js

---
+++ js/index.js
@@ -0,0 +1,123 @@
+
+$("#work").on({mouseenter: function (){
+    $("#work").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#work").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#about").on({mouseenter: function (){
+    $("#about").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#about").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#resume").on({mouseenter: function (){
+    $("#resume").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#resume").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$(".button").on({mouseenter: function (){
+    $(".button").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ (".button").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$(".projectTitleRight").on({mouseenter: function (){
+    $(".projectTitleRight").css({
+        "font-size":"32px"
+    });
+},
+    mouseleave: function () {
+        $ (".projectTitleRight").css ({
+            "font-size":"24px"
+        });
+    }
+
+});
+
+$(".projectTitleLeft").on({mouseenter: function (){
+    $(".projectTitleLeft").css({
+        "font-size":"32px"
+    });
+},
+    mouseleave: function () {
+        $ (".projectTitleLeft").css ({
+            "font-size":"24px"
+        });
+    }
+
+});
+
+$("#headerIcon").on({mouseenter: function (){
+    $("#headerIcon").css({
+        "height":"55px",
+        "width":"55px"
+    });
+},
+    mouseleave: function () {
+        $ ("#headerIcon").css ({
+            "height":"45px",
+            "width":"45px"
+        });
+    }
+
+});
+
+$("#footerIcon").on({mouseenter: function (){
+    $("#footerIcon").css({
+        "height":"55px",
+        "width":"55px"
+    });
+},
+    mouseleave: function () {
+        $ ("#footerIcon").css ({
+            "height":"45px",
+            "width":"45px"
+        });
+    }
+
+});
+
+$("#emailIcon").on({mouseenter: function (){
+    $("#emailIcon").css({
+        "height":"55px",
+        "width":"55px"
+    });
+},
+    mouseleave: function () {
+        $ ("#emailIcon").css ({
+            "height":"45px",
+            "width":"45px"
+        });
+    }
+
+});
